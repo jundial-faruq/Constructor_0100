2db9ee10e2d6b9677a225a0c651fb67c7f9e687e
JundiAlfaruq <jundialfaruq6@gmail.com>
2025-05-13 16:05:01 +0700

    membuat parameternya

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -5,6 +5,12 @@ class mahasiswa{
     public:
         mahasiswa (int nim, string nama);
     };
+
+    mahasiswa::mahasiswa(int nim, string nama){
+        cout << "Constructor Dengan Berparameter Terpanggil" << endl;
+        cout << "NIM    : " << nim << endl;
+        cout << "Nama   : " << nama << endl;
+    }
     
 int main(){
    return 0;
